13bccf4da14dfad071731f391a8b9b55779a5416
hanntronico <hanntronic@hotmail.com>
2013-08-01 12:02:18 -0500

    cambio menu presentacion

--- izq_presentacion.php
+++ izq_presentacion.php
@@ -1,9 +1,23 @@
 	<div id="izquierda">
 
+		<div id="logo_izq">
+			<img src="imagenes/escuela_postgrado.jpg" width="197" height="55" alt="">
+		</div>
+
 		<?php //include ("logo_altagora.php"); ?>
 
-		<?php include ("menu_prese.php"); ?>
+		<?php //include ("menu_prese.php"); ?>
 
-		<div id="spcmenu"></div>
+		<div id="menuizq_pres">
+			<ul>
+			    <li><a href="#"><div id="opc">Autoridades</div></a></li>
+			    <li><a href="#"><div id="opc">Historia</div></a></li>
+			    <li><a href="#"><div id="opc">Doble Grado</div></a></li>
+			    <li><a href="#"><div id="opc">Convenios</div></a></li>
+			    <li><a href="#"><div id="opc">Filial Jaén</div></a></li>
+			</ul>
+		</div>	
+
+		<!-- <div id="spcmenu"></div> -->
 
 	</div>
