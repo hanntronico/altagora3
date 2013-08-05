fc3e08f768090288ef62902f00fa5c4cef9eb0e5
hanntronico <hanntronic@hotmail.com>
2013-08-05 15:58:41 -0500

    subida de 05/082013

---
+++ mag_der_penal.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>ALTAGORA</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+<script src="flash/Scripts/swfobject_modified.js" type="text/javascript"></script>
+</head>
+
+<body>
+
+	<div id="contenedor">
+		<?php include ("cabecera.php"); ?>
+		<?php include ("izq_mag_admin.php"); ?>
+		
+		<div id="cuerpo">
+			<h1>Maestría en Derecho Penal y Procesal Penal</h1>
+			<div id="foto">
+				<img src="imagenes/foto_mag_admin.jpg" alt="foto ciudad"> <br><br>
+			</div>
+			<br>
+			
+			<div id="texto_magadmin">
+				<h2>Presentación </h2>
+				<p>La maestría en Derecho Penal y Procesal Penal, busca desarrollar en sus participantes una perspectiva interdisciplinaria, promoviendo la formación de nuevos investigadores y profesionales con especial énfasis en el conocimiento de la dogmatica penal con los principios derivados del modelo de estado social y democrático de derecho que adopta la constitución y los fundamentos del sistema de derechos humanos.</p>
+
+				<h2>Objetivos: </h2>
+				<ul>
+				    <li>Promover la formación  de nuevos investigadores y profesionales en derecho penal con especial énfasis en el conocimiento de la dogmatica penal.</li>
+				    <li>Contribuir a que los participantes vinculen la interpretación penal con los principios derivados del modelo de estado social y democrático de derecho que adopta la constitución y los fundamentos del sistema de derechos humanos.</li>
+				    <li>Generar conciencia de actitud en el abogado litigante para mejorar sistemas y procedimientos, ordenamiento e instituciones procesales en el Perú. </li>
+				</ul>
+
+				<h2>Dirigido A:</h2>
+				<ul>
+				    <li>Profesionales del derecho.</li>
+				    <li>Abogados defensores en el área penal con especialización en derecho procesal penal.</li>
+				    <li>Magistrados, jueces, vocales o fiscales en el área procesal penal.</li>
+				    <li>Consultores o asesores en materia penal y procesal penal.</li>
+				    <li>Investigadores o docentes de derecho penal y derecho procesal penal.</li>
+				    <li>Profesionales interesados en actualizarse en la regulación contenida en el nuevo código penal y el proceso penal acusatorio.</li>
+				</ul>
+
+				<!-- <img src="imagenes/curricula_magadmin.jpg" alt="foto ciudad"> -->
+
+				
+				<table border="0" cellpadding="1" cellspacing="8" width="700">
+					<tr>
+						<td rowspan="2" bgcolor="#84AFCE" class="tblblanco">CURSOS <br> CICLO I</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derechos humanos, constitución y derecho penal</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Teoría del delito</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Penal Contemporáneo</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Procesal Penal I</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Marketing Jurídico</td>
+						<td bgcolor="#84AFCE" class="tblblanco2" colspan="2">TOTAL HORAS CREDITOS</td>
+					</tr>
+					<tr>
+						<td class="tblminiB">28 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">96</td>
+						<td class="tblminiB">16&nbsp;&nbsp;</td>
+					</tr>
+
+					<tr>
+						<td rowspan="2" bgcolor="#84AFCE" class="tblblanco">CURSOS <br> CICLO II</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Política Criminal</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Liderazgo y Eficacia Personal</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Penal Especial</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Psicología Criminal</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Procesal Penal II</td>
+						<td bgcolor="#84AFCE" class="tblblanco2" colspan="2">TOTAL HORAS CREDITOS</td>
+					</tr>
+					<tr>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">96</td>
+						<td class="tblminiB">16&nbsp;&nbsp;</td>
+					</tr>
+					<tr>
+						<td rowspan="2" bgcolor="#84AFCE" class="tblblanco">CURSOS <br> CICLO III</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Penitenciario</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Penal Económico y de la Empresa</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Criminología</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Aspectos penales del Dº Concursal, prop. intelectual y competencia</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Penal Empresarial</td>
+						<td bgcolor="#84AFCE" class="tblblanco2" colspan="2">TOTAL HORAS CREDITOS</td>
+					</tr>
+					<tr>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">96</td>
+						<td class="tblminiB">16&nbsp;&nbsp;</td>
+					</tr>
+					<tr>
+						<td rowspan="2" bgcolor="#84AFCE" class="tblblanco">CURSOS<br>CICLO IV</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Medicina legal</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Penal Virtual</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Penal Financiero</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">Derecho Penal Internacional</td>
+						<td bgcolor="#91D8F7" class="tblnegroB">La Reparación Civil en el Dº Penal</td>
+						<td bgcolor="#84AFCE" class="tblblanco2" colspan="2">TOTAL HORAS CREDITOS</td>
+					</tr>
+					<tr>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">24 HORAS - 4 CRÉDITOS</td>
+						<td class="tblminiB">96</td>
+						<td class="tblminiB">16&nbsp;&nbsp;</td>
+					</tr>
+
+					<tr>
+						<td rowspan="2">&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+						<td class="tblminiB">384</td>
+						<td class="tblminiB">54</td>
+					</tr>
+
+				</table>
+				
+				
+
+
+<!--			<table border="0" cellpadding="0" cellspacing="0" width="100%">
+					<tr>
+						<td width="48%">
+							<div class="titu_prgmaestrias">Maestrías</div>
+							<ul>
+							    <li>Maestría en Administración de Negocios</li>
+							    <li>Maestría en Derecho Penal y Procesal Penal</li>
+							    <li>Maestría en Derecho Civil y de la Empresa</li>
+							    <li>Maestría en Docencia Universitaria y Gerencia Educativa</li>
+							    <li>Maestría en Tributación y Gestión Fiscal</li>
+							</ul>
+						</td>
+						<td width="10"></td>
+						<td width="48%">
+							<div class="titu_prgdoctorados">Doctorados</div>
+							<ul>
+							    <li>Doctorado en Administración y Dirección de Empresas</li>
+							    <li>Doctorado en Gerencia de Salud</li>
+							    <li>Doctorado en Ciencias de la Educación</li>
+							    <li>&nbsp;</li>
+							    <li>&nbsp;</li>
+							</ul>	
+						</td>
+					</tr>
+				</table> -->
+
+			</div>
+		</div>
+
+		<br>
+
+		<?php include ("pie.php"); ?>
+		<br><br><br>
+	</div>
+
+
+
+
+</body>
+</html>
--- menu_prog.php
+++ menu_prog.php
@@ -10,7 +10,7 @@
 				    <a href="#"><li>Docentes</li></a>
 				    <a href="#"><li>M.B.A. Internacional</li></a>
 					<a href="mag_admin.php"><li>Maestría en Administración de Negocios</li></a>
-					<a href="#"><li>Maestría en Derecho Penal y Procesal Penal</li></a>
+					<a href="mag_der_penal.php"><li>Maestría en Derecho Penal y Procesal Penal</li></a>
 					<a href="#"><li>Maestría en Derecho Civil y de la Empresa</li></a>
 					<a href="#"><li>Maestría en Docencia Universitaria y <br> Gerencia Educativa</li></a>
 					<a href="#"><li>Maestría en Tributación y Gestión Fiscal</li></a>
